4366f12d4a1b94178afde7808ae8f81b0fbae11e
tom <tomallpress@hotmail.com>
2018-01-04 10:05:52 +0000

    cleared up the listings page, and collected each listing

--- src/bearbnbJquery.js
+++ src/bearbnbJquery.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
   updateList = function() {
     $(document).ready(function(){
       var listing = bearbnb.listingsArray[bearbnb.listingsArray.length - 1]
-        $("#list_of_listings").append("Property Name: " + listing.name + '<br>' +
+        $("#list_of_listings").append("<li>Property Name: " + listing.name + '<br>' +
                                       " Description: " + listing.bio + '<br>' +
-                                      " Number of Guests: " + listing.guests + '<br><br>');
+                                      " Number of Guests: " + listing.guests + '<br><br></li>');
     });
   }
 
